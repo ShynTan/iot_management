db10f94bb592349cb3a0cd501131e33e8ed8f5ec
Andreana Tan <tanshyn1999@gmail.com>
2022-05-23 22:36:38 +0800

    Enter'

--- app/Http/Controllers/Auth/test.php
+++ app/Http/Controllers/Auth/test.php
@@ -1,42 +1,24 @@
 <?php
 class AdminController extends Controller
 {
-    function edit_events(Request $request){
-        $id = $request->route('id');
-        $event = DB::select('select * from event where event_id = "' . $id . '"');
-        return view('admin.view_event')->with('event', $event);
+    public function viewProfile()
+    {
+        $id = Auth::user()->id;
+        $user = DB::select('select * from users where id = "' . $id . '"');
+        $result = false;
+        return view('user.profile')->with('user', $user)->with('result', $result);
     }
 
-    function edit_event(Request $request)
+    public function updateuserinfo(Request $request)
     {
-        $query = DB::table('event')
-            ->where(
-                'event_id',
-                $request->input('id')
-            )
-            ->update([
-                "install_date" => $request->input('install_date'),
-                "install_time" => $request->input('install_time'),
-                "pic_id" => $request->input('pic_id'),
-                "pic_name" => $request->input('pic_name'),
-                "device_id" => $request->input('device_id'),
-                "location_id" => $request->input('location_id'),
-                "reading_id" => $request->input('reading_id'),
-                "status_id" => $request->input('status_id')
-            ]);
+        $id = $request->input('id');
+        $email = $request->input('email');
+        $name = $request->input('name');
 
-        if ($query) {
-            return back()->with('success', 'Record has been successfully updated');
-        } else {
-            return back()->with('fail', 'Something went wrong');
-        }
-    }
+        $result = DB::update('update users set email = ?, name=? where id = ?',[$email,$name,$id]);
 
-    function view_events()
-    {
-        $data = array(
-            'list' => DB::table('event')->get()
-        );
-        return view('admin.view_events', $data);
+        $user = DB::select('select * from users where id = "' . $id . '"');
+        return view('user.profile')->with('user', $user)->with('result', $result);
+
     }
 }

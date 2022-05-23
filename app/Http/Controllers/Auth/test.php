6de0be7afd66f108ecaf1fd7c47b23d42b6a997f
Andreana Tan <tanshyn1999@gmail.com>
2022-05-23 22:36:24 +0800

    Enter

--- app/Http/Controllers/Auth/test.php
+++ app/Http/Controllers/Auth/test.php
@@ -1,15 +1,42 @@
 <?php
 class AdminController extends Controller
 {
-    function archiveDeviceAction(Request $request){
+    function edit_events(Request $request){
         $id = $request->route('id');
+        $event = DB::select('select * from event where event_id = "' . $id . '"');
+        return view('admin.view_event')->with('event', $event);
+    }
 
-        $res=DB::update("UPDATE devices SET archive = '1' WHERE device_id = ?;", [$id]);
+    function edit_event(Request $request)
+    {
+        $query = DB::table('event')
+            ->where(
+                'event_id',
+                $request->input('id')
+            )
+            ->update([
+                "install_date" => $request->input('install_date'),
+                "install_time" => $request->input('install_time'),
+                "pic_id" => $request->input('pic_id'),
+                "pic_name" => $request->input('pic_name'),
+                "device_id" => $request->input('device_id'),
+                "location_id" => $request->input('location_id'),
+                "reading_id" => $request->input('reading_id'),
+                "status_id" => $request->input('status_id')
+            ]);
 
-        if($res){
-            return back()->with('success', 'Device Type ID ' . $request->route('id') . " deleted!");
-        }else{
-            echo 'haha' . $request->route('id');
+        if ($query) {
+            return back()->with('success', 'Record has been successfully updated');
+        } else {
+            return back()->with('fail', 'Something went wrong');
         }
+    }
+
+    function view_events()
+    {
+        $data = array(
+            'list' => DB::table('event')->get()
+        );
+        return view('admin.view_events', $data);
     }
 }

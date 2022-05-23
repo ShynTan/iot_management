628a23dd1c00c8ef02f1617eb1faeb0709816948
Andreana Tan <tanshyn1999@gmail.com>
2022-05-23 21:44:40 +0800

    Enter

--- app/Http/Controllers/Auth/test.php
+++ app/Http/Controllers/Auth/test.php
@@ -1,19 +1,36 @@
 <?php
 class AdminController extends Controller
 {
-public function updateApprovalView(Request $request)
-{
-    $id = $request->route('id');
-    $user = DB::select('select * from users where id = "' . $id . '"');
-    return view('admin.userUpdate')->with('user', $user);
-}
+    function edit_device(Request $request)
+    {
+        $query = DB::table('devices')
+            ->where(
+                'id',
+                $request->input('id')
+            )
+            ->update([
+                "device_id" => $request->input('device_id'),
+                "device_type" => $request->input('device_type'),
+                "device_unit" => $request->input('device_unit'),
+                "device_reading" => $request->input('device_reading'),
+                "device_location" => $request->input('device_location'),
+                "floor_level" => $request->input('floor_level'),
+                "window_id" => $request->input('window_id'),
+                "door_id" => $request->input('door_id'),
+                "device_status" => $request->input('device_status'),
+                "device_health" => $request->input('device_health'),
+                "device_uptime" => $request->input('device_uptime'),
+                "device_ip" => $request->input('device_ip'),
+                "device_subnet" => $request->input('device_subnet'),
+                "user_id" => $request->input('user_id'),
+                "device_name" => $request->input('device_name'),
+                "updated_at" => date('Y-m-d')
+            ]);
 
-public function updateApproval(Request $request)
-{
-    $id = $request->input('id');
-    $name = $request->input('name');
-    $approval = $request->input('approval');
-    DB::update('update users set approved = ? where id = ?', [$approval, $id]);
-    return redirect()->route('updateUserApproval.user');
-}
+        if ($query) {
+            return back()->with('success', 'Record has been successfully updated');
+        } else {
+            return back()->with('fail', 'Something went wrong');
+        }
+    }
 }

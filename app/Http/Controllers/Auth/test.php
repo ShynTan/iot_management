062a2ffa59706d5080bd22db328fd45e2dbeb885
Andreana Tan <tanshyn1999@gmail.com>
2022-05-23 18:03:31 +0800

    Enter

--- app/Http/Controllers/Auth/test.php
+++ app/Http/Controllers/Auth/test.php
@@ -1,23 +1,21 @@
 <?php
-class EventController extends Controller
+class AdminController extends Controller
 {
-public function add_event(Request $request)
+public function updateApprovalView(Request $request)
 {
-    $query = DB::table('event')->insert([
-        "install_date" => $request->input('install_date'),
-        "install_time" => $request->input('install_time'),
-        "pic_id" => $request->input('pic_id'),
-        "pic_name" => $request->input('pic_name'),
-        "device_id" => $request->input('device_id'),
-        "location_id" => $request->input('location_id'),
-        "reading_id" => $request->input('reading_id'),
-        "status_id" => $request->input('status_id')
-    ]);
+    $id = $request->route('id');
+    $user = DB::select('select * from users where id = "' . $id . '"');
+    return view('admin.userUpdate')->with('user', $user);
+}
 
-    if ($query) {
-        return back()->with('success', 'Record has been successfully inserted');
-    } else {
-        return back()->with('fail', 'Something went wrong');
-    }
+public function updateApproval(Request $request)
+{
+    $id = $request->input('id');
+    $name = $request->input('name');
+    $approval = $request->input('approval');
+    // var_dump($id);
+    // $user = DB::select('select * from users where id = "'.$id.'"');
+    DB::update('update users set approved = ? where id = ?', [$approval, $id]);
+    return redirect()->route('updateUserApproval.user');
 }
 }

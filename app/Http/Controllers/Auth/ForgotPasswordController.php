e9a6b632fb94a2c28132f0b9455e57bfdd06c779
Andreana Tan <tanshyn1999@gmail.com>
2022-05-25 18:16:27 +0800

    Enter

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -8,9 +8,9 @@ use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 class ForgotPasswordController extends Controller
 {
     /*
-    |--------------------------------------------------------------------------
+    |-
     | Password Reset Controller
-    |--------------------------------------------------------------------------
+    |-
     |
     | This controller is responsible for handling password reset emails and
     | includes a trait which assists in sending these notifications from
